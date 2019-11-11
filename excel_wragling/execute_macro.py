a10a22f37f5d607b7f13fc10cd4215eed4f9ce39
shibaken2017 <kentaro.shibata@datasection.co.jp>
2019-11-11 14:49:25 +0900

    コメントとかの追加

--- excel_wragling/execute_macro.py
+++ excel_wragling/execute_macro.py
@@ -3,12 +3,15 @@ import os
 import openpyxl as px
 import os
 from typing import List
-
+import time
+from retrying import retry
 CURRENT_DIR = os.getcwd()
+FNAME="MUST_CHECK.txt"
 
 
+@retry(stop_max_attempt_number=3)
 def execute_macro(fname: str, macro_name: str):
-    #open file and execute a excel-macro
+    # open file and execute a excel-macro
     print("{}を実行".format(macro_name))
     excel = win32com.client.Dispatch("Excel.Application")  # インスタンス生成
     excel.Visible = False  # エクセルを表示する設定（Falseにすれば非表示で実行される）
@@ -20,35 +23,44 @@ def execute_macro(fname: str, macro_name: str):
     excel.Application.Quit()  # 終了
 
 
-def remove_needless_cell(fname: str,sheet_name="CVSSレベル取得"):
-	#delete neddles line from excel
+def remove_needless_cell(fname: str, sheet_name="CVSSレベル取得"):
+	# delete neddles line from excel
 	print("不要な行を削除")
-	wb = px.load_workbook(fname, data_only=True,keep_vba=True)
+	wb = px.load_workbook(fname, data_only=True, keep_vba=True)
     # titles = wb.get_sheet_names()
 	sheet = wb.get_sheet_by_name(sheet_name)
 	row_: int = 0
 	col_: int = 12
-	index_list=[]
-	for i in range(sheet.max_row):
-		if row_>sheet.max_row:
-			break
+	index_list = []
+	max=sheet.max_row
+	print(max)
+	#time.sleep(2)
+	while  row_<=max:
 		row_ += 1
-		if sheet.cell(row=row_, column=col_).value:
-			row_+=2
+		content=sheet.cell(row=row_, column=col_).value
+		if content:
+			if not content.lower().__contains__("cvss"):
+				with open(FNAME,"a",encoding="utf-8")as writer:
+					print("要確認:{} {}行目!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!".format(FNAME,row_))
+					print(content)
+					print("")
+					writer.write(FNAME+"\t"+str(row_)+"行目\t"+content+"\n")
+
+			row_ += 2
 			index_list.append(row_)
 
-	print(index_list)
+	#print(index_list)
 	wb.close()
-	#data_onlyにして書き換え
-	wb = px.load_workbook(fname,keep_vba=True)
+	# data_onlyにして書き換え
+	wb = px.load_workbook(fname, keep_vba=True)
 	sheet = wb.get_sheet_by_name(sheet_name)
 	for row_ in index_list:
-		sheet.cell(row=row_,column=col_).value=None
+		sheet.cell(row=row_, column=col_).value = None
+	wb.save(os.path.join(CURRENT_DIR, fname))
 
-	wb.save(os.path.join(CURRENT_DIR,fname))
 
 if __name__ == "__main__":
-	import time
+    # macro実行
 	start=time.time()
 	fname_list = os.listdir()
 	for fname in fname_list:
@@ -56,7 +68,7 @@ if __name__ == "__main__":
    			print("#######################################s")
    			print(fname)
    			print("#######################################s")
-   			#execute_macro(fname, "データ削除")
+   			# execute_macro(fname, "データ削除")
    			execute_macro(fname, "バッチログコピー")
    			remove_needless_cell(fname)
    			execute_macro(fname,"ボタン2_Click")
